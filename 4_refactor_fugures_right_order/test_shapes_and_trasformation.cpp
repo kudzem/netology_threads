4f97e96be615f0a8c0977907b36cec3a2bf8c381
Alexander Kokrov <kudzem@mail.ru>
2023-06-18 13:41:31 +0200

    Return back correct check values for square and cube
    Now calculation of cube and square volume and sqaure implemented incorrectly
    as it's incorrectly suposses that two first points are diagonal points
    
    But looks like user is free to use any order of points
    We need to change cube construction interface before we fix that behavior

--- 4_refactor_fugures_right_order/test_shapes_and_trasformation.cpp
+++ 4_refactor_fugures_right_order/test_shapes_and_trasformation.cpp
@@ -36,10 +36,8 @@ int main() {
 	}
 
 	check_main_shape_parameters_match(line, 0, 0, 0, 0, 0);
-	//check_main_shape_parameters_match(square, 1, 0, 1, 0, 0);
-	check_main_shape_parameters_match(square, 1, 0, 0, 0, 0);
-	//check_main_shape_parameters_match(cube, 2, 1, 6, 0, 0);
-	check_main_shape_parameters_match(cube, 2, 0, 0, 0, 0);
+	check_main_shape_parameters_match(square, 1, 0, 1, 0, 0);
+	check_main_shape_parameters_match(cube, 2, 1, 6, 0, 0);
 	check_main_shape_parameters_match(circle, 3, 0, M_PI*2*2, 0, 2);
 	check_main_shape_parameters_match(cylinder, 4, 2 * M_PI * 2 * 5, (2 * M_PI * 2 * 2) + (2 * M_PI * 2 * 5), 5, 2);
 
