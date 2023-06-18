274a5129706ed4522193b954c559244a6e31cc61
Alexander Kokrov <kudzem@mail.ru>
2023-06-18 19:31:09 +0200

    Simplified shape shift function

--- 4_refactor_fugures_right_order/transform.cpp
+++ 4_refactor_fugures_right_order/transform.cpp
@@ -8,28 +8,11 @@ transform::transform(const Shape& sh)
 Shape transform::shift(int m, int n, int k)
 {
 	ThreeDPoint shiftObject(m, n, k);
-	switch (shape.getType())
-	{
-	case Shape::line:
-		shape.points[0] = shape.points[0] + shiftObject;
-		shape.points[1] = shape.points[1] + shiftObject;
-		break;
-	case Shape::circle:
-	case Shape::cylinder:
-		shape.points[0] = shape.points[0] + shiftObject;
-		break;
-	case Shape::sqr:
-		shape.points[0] = shape.points[0] + shiftObject;
-		shape.points[1] = shape.points[1] + shiftObject;
-		shape.points[2] = shape.points[2] + shiftObject;
-		break;
-	case Shape::cube:
-		shape.points[0] = shape.points[0] + shiftObject;
-		shape.points[1] = shape.points[1] + shiftObject;
-		shape.points[2] = shape.points[2] + shiftObject;
-		shape.points[3] = shape.points[3] + shiftObject;
-		break;
+
+	for (auto& p : shape.points) {
+		p = p + shiftObject;
 	}
+
 	return shape;
 }
 
